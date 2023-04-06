39c20e1b65e05611854d2e40039ad27ba92e5982
Jesus Figuera <subergabo@gmail.com>
2023-04-06 14:49:31 -0430

    comEnd

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const text = document.getElementById("text");
 const btn = document.getElementById("btn");
+const elem = document.getElementById("elem");
 var questions = parseInt(prompt("¿cuantas actividades tienes en mente?"));
 var actividades = [];
 btn.addEventListener("click", re);
 var p = true;
+var elemtex = ""
+
 
 for (let i = 0; i < questions; i++){
-	let con = prompt("diga una actividad");
+	let con ={ title: prompt("diga una actividad"), cuenta: 0}
 	actividades.push(con);
 	con = ""
 }
 
+
+
+
 function re(){
-	var resul = "";
+	elemtex = "";
+	var resul;
 	var resul = actividades[Math.floor(Math.random() * actividades.length)];
-	text.innerText=resul;
+	text.innerText=resul.title;
+	resul.cuenta++;
+	actividades.forEach(function(item, index){
+	elemtex += item.title + ": " + item.cuenta + "\n";
+	})
+
+	elem.innerText=elemtex;
+
 	if (p == true){
 		text.style="animation-name:inan"
 		p = false;
@@ -24,7 +38,11 @@ function re(){
 		p = true;
 	}
 
+
 	
 }
+
+
+
 
 
